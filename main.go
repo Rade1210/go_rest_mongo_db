bc6b9a5b26c60842d2b6ba59b374e400d0aa0ea1
Rade1210 <radepetrovic200@gmail.com>
2023-06-04 11:16:45 +0200

    Test POST

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
+type Person struct {
+	FirstName string `json:"firstName"`
+	LastName string `json:"lastName"`
+	PhoneNumber string `json:"phoneNumber"`
+	Address Address `json:"address"`
+	EmailAddress string `json:"emailAddress"`
+}
+
+type Address struct {
+	AddressLine1 string `json:"addressLine1"`
+	AddressLine2 string `json:"addressLine2"`
+	City string `json:"city"`
+	State string `json:"state"`
+	ZipCode string `json:"zipCode"`
+	Country string `json:"country"`
+}
+
 func main() {
 	router := gin.Default() // Address: localhost:8080
-	
-	router.GET("/ping", func(ct *gin.Context) {
-		ct.IndentedJSON(http.StatusOK, gin.H{"message": "Ping successful"})
+	router.POST("/postPerson", func(ct *gin.Context) {
+		var person Person
+		if err := ct.BindJSON(&person); err != nil {
+			ct.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Please provide the details in accepted format"})
+		}
+		fmt.Println(person)
 	})
-
 	router.Run()
 }
